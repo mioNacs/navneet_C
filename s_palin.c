7c37d7ae3b7a61045b5ea717d9456788d43f43cd
Navneet Raj <navneet78030@gmail.com>
2023-05-12 06:25:01 +0530

    a bit of optimization

--- s_palin.c
+++ s_palin.c
@@ -2,14 +2,14 @@
 #include<string.h>
 int main()
 {
-char str1[30],str2[30];
-int camp;
-printf(" \nenter a string: ");
+char str1[20],str2[20];
+int cmp;
+printf("\n enter a string: ");
 scanf("%s",str1);
 strcpy(str2,str1);
 strrev(str1);
-camp=strcmp(str1,str2);
-if(camp==0)
+cmp=strcmp(str1,str2);
+if(cmp==0)
 printf("string is palindrome");
 else
 printf("string is not palindrome");
