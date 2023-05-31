e9a95a62ea751470bb9ecac57bbfb37105ad8cb3
Navneet Raj <navneet78030@gmail.com>
2023-05-31 19:16:32 +0530

    creation of to_int function

--- str2num.c
+++ str2num.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 
+int to_int(char[]);
+
 int main()
 {
     char num[10];
-    int N=0,i,c;
+    int N=0;
     printf("enter a number: ");
     scanf("%s",&num);
-    for(i=0;i<10;i++)
+    N=to_int(num);
+    printf("\nNumber is %d.\nNumber * 2 is %d.",N,N*2);
+    return 0;
+}
+
+int to_int(char num[])
+{
+    int N=0,i,c;
+        for(i=0;i<10;i++)
     {
         c=num[i];
         if(c>=48 && c<=57)
@@ -21,9 +31,9 @@ int main()
         else
         {
            printf("\nplease enter a proper number.");
-            break;
+           N=0;
+           exit(0);
         }
     }
-    printf("\nNumber is %d.\nNumber * 2 is %d.",N,N*2);
-    return 0;
+    return N;
 }
